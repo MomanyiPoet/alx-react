efd8d32abf9547e02208a6e15ed4d405dbc45ad0
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-20 20:08:36 +0300

    Adding CSS & Images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -9,4 +9,25 @@ module.exports = {
         path: path.resolve(__dirname, 'public')
     },
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jp?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
