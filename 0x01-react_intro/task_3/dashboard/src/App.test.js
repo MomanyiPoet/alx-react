c39ef2ca957e65a2a0979a8859d4d5a8b4f3cec3
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-22 16:36:26 +0300

    Create React tests

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import App from "./App";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("should render a div with the class App-header", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-header")).toHaveLength(1);
+  });
+
+  it("should render a div with the class App-body", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-body")).toHaveLength(1);
+  });
+
+  it("should render a div with the class App-footer", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-footer")).toHaveLength(1);
+  });
 });
